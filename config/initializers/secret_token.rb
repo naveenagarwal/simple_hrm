13f143ad028f74e86c118e32163249ecd40e3be6
naveenagarwal <naveenagarwal287@gmail.com>
2014-02-10 11:18:45 +0530

    Empty repository - initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleHrm::Application.config.secret_key_base = 'bc896bf39969d0dd951def361a7be515b51290493ae82ecafb0c5902cfaaa9d2e670ec7f6f0fb1c4a0e8340bb23f5f08549bd1c67eab054890d7f21335867efd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SimpleHrm::Application.config.session_store :cookie_store, key: '_simple_hrm_session'
