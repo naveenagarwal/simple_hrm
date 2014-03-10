dc144b5087754d456ac4faa903abcf2665645df1
naveenagarwal <naveenagarwal287@gmail.com>
2014-03-10 13:55:00 +0530

    STI category model

---
+++ app/models/job_category.rb
@@ -0,0 +1,40 @@
+class JobCategory < Category
+  ###############
+  # Includes
+  ###############
+
+  ###############
+  # Constants
+  ###############
+
+  ###############
+  # Accessors
+  ###############
+
+  ###############
+  # Validations
+  ###############
+  validates :name, presence: true, uniqueness: true, allow_blank: false
+
+  ###############
+  # Associations
+  ###############
+  ###############
+  # Class Methods
+  ###############
+
+  ###############
+  # Public API
+  ###############
+
+  ###############
+  # Protected
+  ###############
+  protected
+
+  ###############
+  # Private
+  ###############
+  private
+
+end
