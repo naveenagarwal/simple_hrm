b01d1f4fc052863baebe1652190cde3628797ac0
naveenagarwal <naveenagarwal287@gmail.com>
2014-03-18 10:47:09 +0530

    organization location view complete

--- app/models/organization_location.rb
+++ app/models/organization_location.rb
@@ -1,2 +1,47 @@
 class OrganizationLocation < ActiveRecord::Base
+  ###############
+  # Includes
+  ###############
+
+  ###############
+  # Constants
+  ###############
+
+  ###############
+  # Accessors
+  ###############
+
+  ###############
+  # Validations
+  ###############
+
+  ###############
+  # Associations
+  ###############
+  has_one :address, as: :addressable
+
+  ####################
+  # Nested Attributes
+  ####################
+  accepts_nested_attributes_for :address,
+    reject_if: :all_blank
+
+  ###############
+  # Class Methods
+  ###############
+
+  ###############
+  # Public API
+  ###############
+
+  ###############
+  # Protected
+  ###############
+  protected
+
+  ###############
+  # Private
+  ###############
+  private
+
 end
