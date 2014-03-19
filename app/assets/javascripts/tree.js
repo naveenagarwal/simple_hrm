839c6e5971dbb52c9369b4c0a353b3427c282bf8
naveenagarwal <naveenagarwal287@gmail.com>
2014-03-19 11:29:23 +0530

    tree view implemented

--- app/assets/javascripts/tree.js
+++ app/assets/javascripts/tree.js
@@ -12,9 +12,12 @@ treeView.create = function(url){
 }
 
 treeView.success = function(data){
-  var html = [];
+
+  var html = "";
   var structure = data.structure;
   var parent_ids = data.parent_ids;
+
+  treeView.createParentNode(parent_ids[0], structure[0]);
 
   for(var i = 0; i < parent_ids.length; i++){
     html = treeView.createHTML(parent_ids[i], structure);
@@ -22,16 +25,60 @@ treeView.success = function(data){
   }
 }
 
+treeView.createParentNode = function(parent_id, structure){
+
+  var html = "<ul>";
+
+  html += "<li id='node-" + structure.id + "'>";
+  html += "<a href='#'>" + structure.unit_id + "</a>";
+  html += "</li>";
+  html += "</ul>";
+
+  $("#tree-view").html(html);
+}
+
 treeView.createHTML = function(parent_id, structure){
 
+  var html = "<ul>";
+
+  for(var i = 1; i < structure.length; i++){
+
+    if(parent_id == structure[i].parent_id){
+
+      html += "<li id='node-" + structure[i].id + "'>";
+      html += "<a href='#'>" + structure[i].unit_id + "</a>";
+      html += "<span>";
+      html += "<a class='tree-links-no-border' data-remote=true href='/organization/organization_structures/" + structure[i].id + "/edit'>";
+      html += "<img src='/assets/edit-icon-16.png' class='icon-list has-tooltip' title=' data-original-title='Edit'>";
+      html += "</a>"
+      html += "<a class='tree-links-no-border' data-remote=true data-method='delete' href='/organization/organization_structures/" + structure[i].id + "'>";
+      html += "<img src='/assets/delete-icon-16.png' class='icon-list has-tooltip' title=' data-original-title='Destroy'>";
+      html += "</a>"
+      html += "</span>";
+      html += "</li>";
+
+    }
+
+  }
+
+  html += "</ul>";
+
+  return html;
 }
 
 treeView.insertHTML = function(parent_id, html){
 
+  var liNode = $("li#node-" + parent_id);
+
+  if ( liNode.length > 0){
+    liNode.append(html);
+  }else{
+    $("#tree-view").html(html);
+  }
 }
 
 treeView.fail = function(data){
-  alert("error getting data");
+  alert("Error Getting Data");
 }
 
 
