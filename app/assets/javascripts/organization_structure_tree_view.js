50b8656c7ddd8e53034051752654da4f36497ed0
Naveen Agarwal <naveenagarwal287@gmail.com>
2014-03-23 15:13:25 +0530

    organization structure tree view js modified

--- app/assets/javascripts/organization_structure_tree_view.js
+++ app/assets/javascripts/organization_structure_tree_view.js
@@ -30,14 +30,10 @@ OrganizationStructureTreeView.createParentNode = function(parent_id, structure){
   var html = "<ul>";
 
   html += "<li id='node-" + structure.id + "'>";
-  html += "<a href='#' class='highligh-on-hover has-tooltip' title='" + structure.title_text + "' data-original-title='" + structure.title_text + "'>" + structure.short_name + "</a>";
+  html += OrganizationStructureTreeView.nodeHTML(structure);
   html += "<span>";
-  html += "<a class='tree-links-no-border' data-remote=true href='/organization/organization_structures/new?node_id=" + structure.node_id  + "&parent_id=" + structure.id + "'>";
-  html += "<img src='/assets/new-icon-16.png' class='icon-list has-tooltip' title='New' data-original-title='New'>";
-  html += "</a>"
-  html += "<a class='tree-links-no-border' data-remote=true href='/organization/organization_structures/" + structure.id + "/edit'>";
-  html += "<img src='/assets/edit-icon-16.png' class='icon-list has-tooltip' title='Edit' data-original-title='Edit'>";
-  html += "</a>"
+  html += OrganizationStructureTreeView.newHTML(structure);
+  html += OrganizationStructureTreeView.editHTML(structure);
   html += "</span>";
   html += "</li>";
   html += "</ul>";
@@ -54,17 +50,15 @@ OrganizationStructureTreeView.createHTML = function(parent_id, structure){
     if(parent_id == structure[i].parent_id){
 
       html += "<li id='node-" + structure[i].id + "'>";
-      html += "<a href='#' class='highligh-on-hover has-tooltip' title='" + structure[i].title_text + "' data-original-title='" + structure[i].title_text + "'>" + structure[i].short_name + "</a>";
+
+      html += OrganizationStructureTreeView.nodeHTML(structure[i]);
+
       html += "<span>";
-      html += "<a class='tree-links-no-border' data-remote=true href='/organization/organization_structures/new?node_id=" + structure[i].node_id  + "&parent_id=" + structure[i].id + "'>";
-      html += "<img src='/assets/new-icon-16.png' class='icon-list has-tooltip' title='New' data-original-title='New'>";
-      html += "</a>"
-      html += "<a class='tree-links-no-border' data-remote=true href='/organization/organization_structures/" + structure[i].id + "/edit'>";
-      html += "<img src='/assets/edit-icon-16.png' class='icon-list has-tooltip' title='Edit' data-original-title='Edit'>";
-      html += "</a>"
-      html += "<a class='tree-links-no-border' data-confirm='Are you sure? Units under selected unit will also be deleted?' data-remote=true data-method='delete' href='/organization/organization_structures/" + structure[i].id + "'>";
-      html += "<img src='/assets/delete-icon-16.png' class='icon-list has-tooltip' title='Destroy' data-original-title='Destroy'>";
-      html += "</a>"
+
+      html += OrganizationStructureTreeView.newHTML(structure[i]);
+      html += OrganizationStructureTreeView.editHTML(structure[i]);
+      html += OrganizationStructureTreeView.deleteHTML(structure[i]);
+
       html += "</span>";
       html += "</li>";
 
@@ -77,7 +71,7 @@ OrganizationStructureTreeView.createHTML = function(parent_id, structure){
   return html;
 }
 
-OrganizationStructureTreeView.insertHTML = function(parent_id, html){
+OrganizationStructureTreeView.insertHTML = function (parent_id, html){
 
   var liNode = $("li#node-" + parent_id);
 
@@ -88,8 +82,41 @@ OrganizationStructureTreeView.insertHTML = function(parent_id, html){
   }
 }
 
+OrganizationStructureTreeView.editHTML = function (structure){
+  var html = "";
+
+  html += "<a class='tree-links-no-border' data-remote=true href='/organization/organization_structures/" + structure.id + "/edit'>";
+  html += "<img src='/assets/edit-icon-16.png' class='icon-list has-tooltip' title='Edit' data-original-title='Edit'>";
+  html += "</a>";
+
+  return html;
+}
+
+OrganizationStructureTreeView.newHTML = function (structure){
+  var html = "";
+
+  html += "<a class='tree-links-no-border' data-remote=true href='/organization/organization_structures/new?node_id=" + structure.node_id  + "&parent_id=" + structure.id + "'>";
+  html += "<img src='/assets/new-icon-16.png' class='icon-list has-tooltip' title='New' data-original-title='New'>";
+  html += "</a>";
+
+  return html;
+}
+
+OrganizationStructureTreeView.deleteHTML = function (structure){
+  var html = "";
+
+  html += "<a class='tree-links-no-border' data-confirm='Are you sure? Units under selected unit will also be deleted?' data-remote=true data-method='delete' href='/organization/organization_structures/" + structure.id + "'>";
+  html += "<img src='/assets/delete-icon-16.png' class='icon-list has-tooltip' title='Destroy' data-original-title='Destroy'>";
+  html += "</a>";
+
+  return html;
+}
+
+OrganizationStructureTreeView.nodeHTML = function (structure){
+  var html = "<a href='#' class='highligh-on-hover has-tooltip' title='" + structure.title_text + "' data-original-title='" + structure.title_text + "'>" + structure.short_name + "</a>";
+  return html;
+}
+
 OrganizationStructureTreeView.fail = function(data){
   alert("Error Getting Data");
 }
-
-
