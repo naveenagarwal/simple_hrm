97c711370f13d0788fc8e0863181a37ad0efb894
naveenagarwal <naveenagarwal287@gmail.com>
2014-04-02 11:25:53 +0530

    Model template override

--- lib/generators/active_record/model/templates/model.rb
+++ lib/generators/active_record/model/templates/model.rb
@@ -1,10 +1,58 @@
 <% module_namespacing do -%>
 class <%= class_name %> < <%= parent_class_name.classify %>
+  ###############
+  # Constants
+  ###############
+
+  ###############
+  # Includes
+  ###############
+
+  ###############
+  # Accessors
+  ###############
+
+  ###############
+  # Validations
+  ###############
+
+  ###############
+  # Associations
+  ###############
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
 <% end -%>
 <% if attributes.any?(&:password_digest?) -%>
+
+  ###############
+  # Secure Pass
+  ###############
   has_secure_password
 <% end -%>
+
+  ###############
+  # Nested Attr.
+  ###############
+
+  ###############
+  # Class Methods
+  ###############
+  class << self
+  end
+
+  ###############
+  # Public API
+  ###############
+
+  ###############
+  # Protected
+  ###############
+  protected
+
+  ###############
+  # Private
+  ###############
+  private
+
 end
 <% end -%>
