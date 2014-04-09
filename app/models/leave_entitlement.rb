5110c818ca73fe46780dd7be0667001225fe75aa
naveenagarwal <naveenagarwal287@gmail.com>
2014-04-09 14:33:33 +0530

    relations and validations for leave entitlement

--- app/models/leave_entitlement.rb
+++ app/models/leave_entitlement.rb
@@ -14,10 +14,18 @@ class LeaveEntitlement < ActiveRecord::Base
   ###############
   # Validations
   ###############
+  validates :user, :leave_type, :location, :from, :to, presence: true
+
+  validates_with MinMaxValidator,
+    fields: { min: :from, max: :to, msg: "can't be before \"From\"" }
 
   ###############
   # Associations
   ###############
+  belongs_to :user
+  belongs_to :leave_type
+  belongs_to :location
+
   ###############
   # Class Methods
   ###############
