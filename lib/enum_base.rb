ca7ba689b07204d5daaeb6814d3c739ef2bc5f0e
naveenagarwal <naveenagarwal287@gmail.com>
2014-04-11 12:40:51 +0530

    modified enum base to have the extra options in the intialization method

--- lib/enum_base.rb
+++ lib/enum_base.rb
@@ -1,9 +1,12 @@
 module EnumBase
+
   attr_reader :description
   attr_reader :value
   attr_reader :ordinal
+  attr_reader :options
 
-  def init(description, value, ordinal)
-    @description, @value, @ordinal = description, value, ordinal
+  def init(description, value, ordinal, options={})
+    @description, @value, @ordinal @options = description, value, ordinal, options
   end
+
 end
