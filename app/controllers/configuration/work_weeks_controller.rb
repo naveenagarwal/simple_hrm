b1b47a54ca2a16a835bb36608b228fd1e01a2861
naveenagarwal <naveenagarwal287@gmail.com>
2014-04-17 11:19:09 +0530

    configuration work week changes

--- app/controllers/configuration/work_weeks_controller.rb
+++ app/controllers/configuration/work_weeks_controller.rb
@@ -1,74 +1,56 @@
 class Configuration::WorkWeeksController < ApplicationController
-  before_action :set_configuration_work_week, only: [:show, :edit, :update, :destroy]
+  before_action :set_work_week, only: [:edit, :update, :destroy]
 
-  # GET /configuration/work_weeks
-  # GET /configuration/work_weeks.json
   def index
-    @configuration_work_weeks = Configuration::WorkWeek.all
+    @work_weeks = paginated_records_for WorkWeek
   end
 
-  # GET /configuration/work_weeks/1
-  # GET /configuration/work_weeks/1.json
-  def show
-  end
-
-  # GET /configuration/work_weeks/new
   def new
-    @configuration_work_week = Configuration::WorkWeek.new
+    @work_week = WorkWeek.new
   end
 
-  # GET /configuration/work_weeks/1/edit
   def edit
   end
 
-  # POST /configuration/work_weeks
-  # POST /configuration/work_weeks.json
   def create
-    @configuration_work_week = Configuration::WorkWeek.new(configuration_work_week_params)
+    @work_week = WorkWeek.new(work_week_params)
 
     respond_to do |format|
-      if @configuration_work_week.save
-        format.html { redirect_to @configuration_work_week, notice: 'Work week was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @configuration_work_week }
+      if @work_week.save
+        format.html { redirect_to configuration_work_weeks_path(pagination_params), notice: t("model.create", kind: "Work week") }
       else
         format.html { render action: 'new' }
-        format.json { render json: @configuration_work_week.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /configuration/work_weeks/1
-  # PATCH/PUT /configuration/work_weeks/1.json
   def update
     respond_to do |format|
-      if @configuration_work_week.update(configuration_work_week_params)
-        format.html { redirect_to @configuration_work_week, notice: 'Work week was successfully updated.' }
-        format.json { head :no_content }
+      if @work_week.update(work_week_params)
+        format.html { redirect_to configuration_work_weeks_path(pagination_params), notice: t("model.update", kind: "Work week") }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @configuration_work_week.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /configuration/work_weeks/1
-  # DELETE /configuration/work_weeks/1.json
   def destroy
-    @configuration_work_week.destroy
+    @work_week.destroy
     respond_to do |format|
-      format.html { redirect_to configuration_work_weeks_url }
-      format.json { head :no_content }
+      format.html { redirect_to configuration_work_weeks_path(pagination_params), notice: t("model.destroy", kind: "Work week") }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_configuration_work_week
-      @configuration_work_week = Configuration::WorkWeek.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def configuration_work_week_params
-      params[:configuration_work_week]
-    end
+  def set_work_week
+    @work_week = WorkWeek.find(params[:id])
+  end
+
+  def work_week_params
+    params.require(:work_week).permit(
+      :country, weekdays_info: [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+    )
+  end
+
 end
