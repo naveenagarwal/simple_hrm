135ad124b7b7fb73a26b3869bca3e2e685df3d23
naveenagarwal <naveenagarwal287@gmail.com>
2014-04-17 12:11:12 +0530

    weekday info for work week display on listing page

--- app/helpers/configuration/work_weeks_helper.rb
+++ app/helpers/configuration/work_weeks_helper.rb
@@ -1,2 +1,8 @@
 module Configuration::WorkWeeksHelper
+
+  def format_weekday_info(weekdays_info)
+    weekdays_info.map do |k, v|
+      "#{k}: #{description_for(AppEnumerables::WeekdaysInfo, v.to_i)}"
+    end.join(", ")
+  end
 end
