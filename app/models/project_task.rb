462f4ae3c544faa27b538c0a5eb7ffef712859c2
naveenagarwal <naveenagarwal287@gmail.com>
2014-05-08 10:41:59 +0530

    project tasks model

---
+++ app/models/project_task.rb
@@ -0,0 +1,40 @@
+class ProjectTask < ActiveRecord::Base
+  ###############
+  # Includes
+  ###############
+
+  ###############
+  # Constants
+  ###############
+
+  ###############
+  # Accessors
+  ###############
+
+  ###############
+  # Validations
+  ###############
+
+  ###############
+  # Associations
+  ###############
+  belongs_to :project
+  ###############
+  # Class Methods
+  ###############
+
+  ###############
+  # Public API
+  ###############
+
+  ###############
+  # Protected
+  ###############
+  protected
+
+  ###############
+  # Private
+  ###############
+  private
+
+end
---
+++ db/migrate/20140508051134_create_project_tasks.rb
@@ -0,0 +1,10 @@
+class CreateProjectTasks < ActiveRecord::Migration
+  def change
+    create_table :project_tasks do |t|
+      t.string :name
+      t.references :project, index: true
+
+      t.timestamps
+    end
+  end
+end
