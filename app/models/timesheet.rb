e76c3ff69885829a047988cd45df9bd721ae5928
naveenagarwal <naveenagarwal287@gmail.com>
2014-05-12 14:54:34 +0530

    timesheet and timesshet entry model validations and scopes and public api

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -6,6 +6,7 @@ class Timesheet < ActiveRecord::Base
   ###############
   # Constants
   ###############
+  SEVEN = 7
 
   ###############
   # Accessors
@@ -20,13 +21,43 @@ class Timesheet < ActiveRecord::Base
   ###############
   belongs_to :user
 
+  has_many :timesheet_entries
+
   ###############
   # Class Methods
   ###############
+  class << self
+
+    def get_entries(status: nil, period: nil)
+      status ||= AppEnumerables::TimesheetEntryStatus::Pending.value
+      period ||= Timesheet::SEVEN
+
+      for_date_range = period.to_i.days.ago.to_date..Date.today
+
+      includes(:timesheet_entries).joins(:timesheet_entries).where(
+
+          :"timesheet_entries.status" => status,
+          :"timesheets.for_date"      => for_date_range
+
+        ).order(:for_date)
+    end
+
+  end
 
   ###############
   # Public API
   ###############
+  def pending_entries
+    timesheet_entries.pending
+  end
+
+  def approved_entries
+    timesheet_entries.approved
+  end
+
+  def rejected_entries
+    timesheet_entries.rejected
+  end
 
   ###############
   # Protected
--- app/models/timesheet_entry.rb
+++ app/models/timesheet_entry.rb
@@ -12,8 +12,17 @@ class TimesheetEntry < ActiveRecord::Base
   ###############
 
   ###############
+  # Scopes
+  ###############
+  scope :pending,  -> { where(status: AppEnumerables::TimesheetEntryStatus::Pending.value)  }
+  scope :approved, -> { where(status: AppEnumerables::TimesheetEntryStatus::Approved.value) }
+  scope :rejected, -> { where(status: AppEnumerables::TimesheetEntryStatus::Rejected.value) }
+
+  ###############
   # Validations
   ###############
+  validates :project_id, :project_task_id, :description, :time_spent, :status,
+            presence: true
 
   ###############
   # Associations
